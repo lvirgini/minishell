b21698dd81d9ed9242424d25e586f7adae4245ac
Lakhdari VIRGINIE <lvirgini@e2r1p6.clusters.42paris.fr>
2021-11-19 13:28:13 +0100

    norminette ++

--- srcs/expander/expand_redirection.c
+++ srcs/expander/expand_redirection.c
@@ -13,48 +13,6 @@
 #include "minishell.h"
 
 // redirection ambigue : si $TEST="plusieurs elements"
-/*
-static int	check_redir_expansion(char **expansion, char *filename)
-{
-	size_t	i;
-
-	i = 0;
-	while (expansion[i])
-		i++;
-	if (i > 1)
-	{
-		display_error(ERR_AMBIGUOUS, filename);
-		return (FAILURE);
-	}
-	return (SUCCESS);
-}
-*/
-/*
-t_redir	*expand_redirection(t_redir *redir, char **env)
-{
-	t_redir		*first_redir;
-	char		**expansion;
-
-	first_redir = redir;
-	while (redir)
-	{
-		if (need_expand_str(redir->filename))
-		{
-			expansion = NULL;
-			expansion = expand(redir->filename, env);
-			if (check_redir_expansion(expansion, redir->filename) == FAILURE)
-			{
-				free_list_redir(first_redir);
-				return (NULL);
-			}
-			free(redir->filename);
-			redir->filename = *expansion;
-			free(expansion);
-		}
-		redir = redir->next;
-	}
-	return (first_redir);
-}*/
 
 int	expand_redirection(t_redir *redir, char **env)
 {
