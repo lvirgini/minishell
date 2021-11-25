d25e2cce3b0ae5a70aa62f1bb75437de17ec45a7
Eithan ASSOULINE <eassouli@e2r2p11.clusters.42paris.fr>
2021-11-25 11:45:13 +0100

    set conditions

--- srcs/built_ins/exit.c
+++ srcs/built_ins/exit.c
@@ -6,7 +6,7 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 16:26:37 by eassouli          #+#    #+#             */
-/*   Updated: 2021/11/24 18:03:24 by eassouli         ###   ########.fr       */
+/*   Updated: 2021/11/25 11:40:43 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@ void	exec_exit(char **arg, char ***env)
 {
 	(void)env;
 	(void)arg;
+	// Cast chiffre en unsigned char
+	// Si 0 arg print et return last return value
+	// Si 1 arg chiffre, print et return chiffre
+	// Si 1 arg pas chiffre print error arg1: numeric argument required\n return 2
+	// Si plus arg 0 chiffre et 1 pas chiffre print et error exit: too many arguements\n mais pas stop et return value 1
+	// Si plus arg 0 pas chiffre print arg1: numeric argument required\n stop et return 2
 	//print exit\n set variable globale pour stopper loop
-	// Ctrl + D doit ecrire exit
+	// Ctrl + D doit appeler exit
 }
