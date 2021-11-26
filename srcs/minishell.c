47688efc92813ca248e10b0f62f7faa64b10d47c
Eithan ASSOULINE <eassouli@e2r2p12.clusters.42paris.fr>
2021-11-26 14:25:57 +0100

    handle all signals

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/03 16:25:24 by lvirgini          #+#    #+#             */
-/*   Updated: 2021/11/26 10:14:08 by eassouli         ###   ########.fr       */
+/*   Updated: 2021/11/26 14:24:30 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,15 @@
 ** de mettre "" que NULL ?
 */
 
+void	handle_sigint(int sig)
+{
+	(void)sig;
+	rl_on_new_line();
+	printf("\n");
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
 int	main(int argc, char *argv[], char *env[])
 {
 	char	**minishell_env;
@@ -25,6 +34,8 @@ int	main(int argc, char *argv[], char *env[])
 	(void)argc;
 	(void)argv;
 	minishell_env = make_minishell_env(env); //protection
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, SIG_IGN);
 	make_terminal(&minishell_env);
 	free_list(minishell_env);
 	return (get_exit_status());
